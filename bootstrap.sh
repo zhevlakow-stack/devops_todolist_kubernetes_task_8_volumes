1c42b61452cae78d13f8ef4ea2a28348b02e388f
DESKTOP-FR144OA\Anubis <adamas87@gmail.com>
2026-01-06 16:18:48 +0800

    'Solution'

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# 1. Create Namespace (if not exists)
+kubectl create namespace todoapp --dry-run=client -o yaml | kubectl apply -f -
+
+# 2. Apply Storage Resources
+kubectl apply -f pv.yml
+kubectl apply -f pvc.yml
+
+# 3. Apply Configuration
+kubectl apply -f configMap.yml
+kubectl apply -f secret.yml
+
+# 4. Apply Application
+kubectl apply -f deployment.yml
+
+echo "Bootstrap finished. Resources applied to 'todoapp' namespace."
